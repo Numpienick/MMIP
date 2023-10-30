43d26fe857411ded8389f169ba4c25f4477634c1
Wim <w.j.stienstra@home.nl>
2023-10-30 16:00:14 +0100

    Update playwright.config.ts

--- src/Test/ui-tests/playwright.config.ts
+++ src/Test/ui-tests/playwright.config.ts
@@ -57,11 +57,4 @@ export default defineConfig({
       use: { ...devices["iPhone 12"] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  //webServer: {
-  //  command: 'dotnet run --project ../../Client/Client.csproj',
-  //  url: 'http://127.0.0.1:3000',
-  //  reuseExistingServer: !process.env.CI,
-  //},
 });
