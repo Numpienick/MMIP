91227c6ca3118a447ea50c4e02ec491097a0b7c4
WimStienstra <w.j.stienstra@home.nl>
2023-11-07 13:59:25 +0100

    fix: changed repositories reference, reverted new repositories (wrong project)

--- src/Infrastructure/Services/AuthenticationService.cs
+++ src/Infrastructure/Services/AuthenticationService.cs
@@ -1,4 +1,4 @@
-﻿using Server.Repositories;
+﻿using Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,5 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    public class AuthenticationService
-    {
-    }
+    public class AuthenticationService { }
 }
--- src/Infrastructure/Services/ReactionService.cs
+++ src/Infrastructure/Services/ReactionService.cs
@@ -1,4 +1,4 @@
-﻿using Server.Repositories;
+﻿using Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,5 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    public class ReactionService : BaseEntityService
-    {
-    }
+    public class ReactionService : BaseEntityService { }
 }
