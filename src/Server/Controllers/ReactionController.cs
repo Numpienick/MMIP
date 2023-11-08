4aaa965304a0e405f805817c82a0d2a67126e245
Nick Numan <nicknuman@gmail.com>
2023-11-08 15:08:06 +0100

    build(server): changed Server.csproj to a Web API

--- src/Server/Controllers/ReactionController.cs
+++ src/Server/Controllers/ReactionController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Server.Controllers
 {
-    public class ReactionController
-    {
-    }
+    [ApiController]
+    public class ReactionController { }
 }
