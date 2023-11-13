41456dfd1861e86be47dec6611d35b5fc39319c7
WimStienstra <w.j.stienstra@home.nl>
2023-11-13 12:18:23 +0100

    feat: added mudblazor, cors, challengescontroller, text constructor

---
+++ src/Server/Controllers/ChallengesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChallengesController
+    {
+        [HttpPost]
+        public IActionResult CreateChallenge([FromBody] Challenge challenge)
+        {
+            try
+            {
+                Console.WriteLine("Succesfully created challenge: " + challenge.Title.Value);
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new BadRequestResult();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public static IActionResult GetChallenge(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public static IActionResult GetChallenges(ICriteria filterCriteria)
+        {
+            return null;
+        }
+
+        [HttpPatch]
+        public static IActionResult UpdateChallenge(Challenge challenge)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public static IActionResult DeleteChallenge(Challenge challenge)
+        {
+            return null;
+        }
+    }
+}
--- src/Shared/Entities/Challenge.cs
+++ src/Shared/Entities/Challenge.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace Shared.Entities
 {
     public class Challenge : BaseEntity
     {
+        [Required(ErrorMessage = "Required")]
         public Text Title { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [StringLength(8, ErrorMessage = "Description length needs to be more than 8.")]
         public Text Description { get; set; }
-        public DateTime Deadline { get; set; }
+        public DateTimeOffset Deadline { get; set; }
         public Text FinalReport { get; set; }
     }
 }
