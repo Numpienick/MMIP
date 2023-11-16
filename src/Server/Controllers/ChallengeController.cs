02c358d501ceb2a76835e47568a370722fa17278
WimStienstra <w.j.stienstra@home.nl>
2023-11-16 15:25:25 +0100

    fix: merge conflict

--- src/Server/Controllers/ChallengeController.cs
+++ src/Server/Controllers/ChallengeController.cs
@@ -1,7 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Filters;
 
 namespace Server.Controllers
 {
-    [ApiController]
-    public class ChallengeController { }
+    public class ChallengeController
+    {
+        public IActionResult CreateChallenge()
+        {
+            return null;
+        }
+
+        public IActionResult GetChallenge(Guid id)
+        {
+            return null;
+        }
+
+        public IActionResult GetChallenges(ICriteria filterCriteria)
+        {
+            return null;
+        }
+
+        public IActionResult UpdateChallenge(Challenge challenge)
+        {
+            return null;
+        }
+
+        public IActionResult DeleteChallenge(Challenge challenge)
+        {
+            return null;
+        }
+    }
 }
