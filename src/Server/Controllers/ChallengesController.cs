020ce7477f1e679f64dcf47385ce8969f97f5a89
WimStienstra <w.j.stienstra@home.nl>
2023-11-16 11:45:12 +0100

    feat(MAT-256): added forms, handling, put this in a component, text to string

---
+++ src/Server/Controllers/ChallengesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Entities;
+using System.Text.Json;
+using Shared.Filters;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChallengesController
+    {
+        [HttpPost]
+        public IActionResult CreateChallenge([FromBody] string json)
+        {
+            Challenge challenge = JsonSerializer.Deserialize<Challenge>(json);
+            Console.WriteLine(challenge);
+            try
+            {
+                Console.WriteLine("Succesfully created challenge: " + challenge.Title);
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new BadRequestResult();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public static IActionResult GetChallenge(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public static IActionResult GetChallenges(ICriteria filterCriteria)
+        {
+            return null;
+        }
+
+        [HttpPatch]
+        public static IActionResult UpdateChallenge(Challenge challenge)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public static IActionResult DeleteChallenge(Challenge challenge)
+        {
+            return null;
+        }
+    }
+}
--- src/Shared/Entities/Challenge.cs
+++ src/Shared/Entities/Challenge.cs
@@ -8,5 +8,14 @@ namespace Shared.Entities
 {
     public class Challenge : BaseEntity
     {
+        //TODO: Make it work with Text instead of string
+        [Required(ErrorMessage = "Required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [StringLength(1000, ErrorMessage = "Max description is 1000")]
+        public string Description { get; set; }
+        public DateTimeOffset Deadline { get; set; }
+        public Text FinalReport { get; set; }
     }
 }
