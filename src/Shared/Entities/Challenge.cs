94d6958079502ab4acb7a34baee58981fdcf616f
WimStienstra <w.j.stienstra@home.nl>
2023-11-20 09:28:06 +0100

    feat: createchallenge more fields added

--- src/Shared/Entities/Challenge.cs
+++ src/Shared/Entities/Challenge.cs
@@ -12,10 +12,32 @@ namespace Shared.Entities
         //TODO: Make it work with Text instead of string
         [Required(ErrorMessage = "Required")]
         public string Title { get; set; }
+
         [Required(ErrorMessage = "Required")]
         [StringLength(1000, ErrorMessage = "Max description is 1000")]
         public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public string BannerImagePath { get; set; }
         public DateTimeOffset Deadline { get; set; }
         public string FinalReport { get; set; }
+        public int ChallengeVisibility { get; set; }
+
+        public static IEnumerable<Visibility> Visiblities = new List<Visibility>()
+        {
+            new() { Id = 0, Name = "" },
+            new() { Id = 1, Name = "Zichtbaar alleen voor eigen werknemers" },
+            new()
+            {
+                Id = 2,
+                Name = "Zichtbaar alleen voor eigen werknemers en ingelogd gebruikers"
+            },
+            new() { Id = 3, Name = "Zichtbaar voor iedereen" },
+        };
+    }
+
+    public class Visibility
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
