effda5ed472ef7e008b3f861b872e5209eb9973f
Wim Stienstra <w.j.stienstra@home.nl>
2023-11-20 13:37:40 +0100

    feat(MAT-189): added localization support

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -11,5 +11,6 @@ builder.Services.AddMudServices();
 builder.Services.AddScoped(
     sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 );
+builder.Services.AddLocalization();
 
 await builder.Build().RunAsync();
