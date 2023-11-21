10bd1c25a6045f8a93a7745f9a75377f2bc19b23
Wim <w.j.stienstra@home.nl>
2023-11-21 14:27:07 +0100

    feat(MAT-258): reworked httpclient and using new environment variables and logic (#28)
    * fix: changed ints in ChallengeVisibility to Visibility
    
    * feat(MAT-258): reworked httpclient and using new environment variables and logic
    
    * fix: processed feedback. connection route string fix, launchsetting removed nativedebugging

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+using Environment;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -6,10 +7,9 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddMudServices();
 builder.Services.AddScoped(
-    sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
+    sp => new HttpClient { BaseAddress = new Uri(EnvironmentConstants.ApiUrl) }
 );
 builder.Services.AddLocalization();
 
