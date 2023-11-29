cf10563b5b493edd18b977354a46f92cb9d61423
Wim <w.j.stienstra@home.nl>
2023-11-29 11:21:04 +0100

    Apply suggestions from code review
    Co-authored-by: Nick Numan <43172288+Numpienick@users.noreply.github.com>

--- src/Shared/Entities/BaseEntity.cs
+++ src/Shared/Entities/BaseEntity.cs
@@ -6,7 +6,6 @@ namespace Shared.Entities
     public abstract class BaseEntity
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
         public DateTimeOffset UpdatedDate { get; set; }
