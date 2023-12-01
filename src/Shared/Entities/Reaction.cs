3e74e320b0f83d6830b8162c3b77ddc103467f63
LeanderJelgersma <89790634+LeanderJelgersma@users.noreply.github.com>
2023-12-01 11:30:01 +0100

    feat(MAT-117): converted ReactionType to enum and added a custom attribute (#32)
    * feat(MAT-117): added comments layout
    
    * feat(MAT-117): converted ReactionType to enum and added a custom attribute
    
    * refactor(MAT-117): added localization on hardcoded strings and added dependency injection
    
    * feat(MAT-117): changed card color of a comment based on if it is concluded
    
    * refactor(MAT-117): added exception handling on enumextensions
    
    ---------
    
    Co-authored-by: Leander Jelgersma <Leanderj123@outlook.com>

--- src/Shared/Entities/Reaction.cs
+++ src/Shared/Entities/Reaction.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared.Enums;
 
 namespace Shared.Entities
 {
     public class Reaction : BaseEntity
     {
+        public string Text { get; set; }
+        public bool Concluded { get; set; }
+        public ReactionType ReactionType { get; set; }
     }
 }
--- src/Shared/Utilities/EnumExtensions.cs
+++ src/Shared/Utilities/EnumExtensions.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +27,34 @@ namespace Shared.Utilities
             }
 
             return enumValue.ToString();
+        }
+
+        public static string GetReactionTypePropertyValue(this Enum value, string propertyName)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+
+            try
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    if (
+                        Attribute.GetCustomAttribute(field, typeof(ReactionTypeAttribute))
+                        is ReactionTypeAttribute attribute
+                    )
+                    {
+                        var propertyInfo = typeof(ReactionTypeAttribute).GetProperty(propertyName);
+                        return propertyInfo.GetValue(attribute, null).ToString();
+                    }
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                throw;
+            }
+
+            return name;
         }
     }
 }
