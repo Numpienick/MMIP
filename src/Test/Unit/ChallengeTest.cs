bd457ea92ed9645932eb1ecdccdf079648ee63c9
WimStienstra <w.j.stienstra@home.nl>
2023-12-04 10:32:29 +0100

    feat: added simple unit tests + split statuscode strings from post in controller

---
+++ src/Test/Unit/ChallengeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Shared.Entities;
+using Shared.Enums;
+using Client.Controllers;
+using Infrastructure.Services;
+using Moq;
+using MudBlazor;
+
+namespace Test.Unit
+{
+    public class ChallengeTest
+    {
+        [Fact]
+        public void IsDescriptionSizeValid()
+        {
+            string longString = new string('a', unchecked(0x0FFFFFF));
+
+            var challenge = new Challenge
+            {
+                Title = "Test",
+                FinalReport = "Final Report",
+                ShortDescription = "Short Description",
+                Description = longString,
+                ChallengeVisibility = Visibility.VisibleToAll,
+                Deadline = DateTime.Now
+            };
+            ChallengeService challengeService = new ChallengeService();
+            challengeService.CreateChallenge(challenge);
+        }
+    }
+}
---
+++ src/Test/Unit/RequestTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Controllers;
+using Client.Resources;
+using Moq;
+using MudBlazor;
+
+namespace Test.Unit
+{
+    public class RequestTest
+    {
+        [Fact]
+        public void ServiceNotFoundReturnsCorrectString()
+        {
+            var snackbar = Mock.Of<ISnackbar>();
+            var httpClient = Mock.Of<HttpClient>();
+            RequestController requestController = new RequestController(snackbar, httpClient);
+
+            Assert.Equal(
+                ApplicationResource.Request_NotFound,
+                requestController.GetStringStatusCode(HttpStatusCode.NotFound)
+            );
+        }
+
+        [Fact]
+        public void ServiceUnavailableReturnsCorrectString()
+        {
+            var snackbar = Mock.Of<ISnackbar>();
+            var httpClient = Mock.Of<HttpClient>();
+            RequestController requestController = new RequestController(snackbar, httpClient);
+
+            Assert.Equal(
+                ApplicationResource.Request_ServiceUnavailable,
+                requestController.GetStringStatusCode(HttpStatusCode.ServiceUnavailable)
+            );
+        }
+
+        [Fact]
+        public void RequestTooLargeReturnsCorrectString()
+        {
+            var snackbar = Mock.Of<ISnackbar>();
+            var httpClient = Mock.Of<HttpClient>();
+            RequestController requestController = new RequestController(snackbar, httpClient);
+
+            Assert.Equal(
+                ApplicationResource.Request_TooLarge,
+                requestController.GetStringStatusCode(HttpStatusCode.RequestEntityTooLarge)
+            );
+        }
+
+        [Fact]
+        public void NotAcceptableReturnsCorrectString()
+        {
+            var snackbar = Mock.Of<ISnackbar>();
+            var httpClient = Mock.Of<HttpClient>();
+            RequestController requestController = new RequestController(snackbar, httpClient);
+
+            Assert.Equal(
+                ApplicationResource.Request_ServiceUnavailable,
+                requestController.GetStringStatusCode(HttpStatusCode.ServiceUnavailable)
+            );
+        }
+    }
+}
