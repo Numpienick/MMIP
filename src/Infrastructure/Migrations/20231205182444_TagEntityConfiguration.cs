5de2b7e905ef9fa09e4f181d518eb2fdb36e7e06
Nick Numan <nicknuman@gmail.com>
2023-12-05 20:30:10 +0100

    fix(migration): broken migration regarding challenge_tags
    not sure what happened because it was working, but a hidden migration needed to be added.

--- src/Infrastructure/Migrations/20231205182444_TagEntityConfiguration.cs
+++ src/Infrastructure/Migrations/20231205182444_TagEntityConfiguration.cs
@@ -55,47 +55,11 @@ namespace Infrastructure.Migrations
                 column: "value",
                 unique: true
             );
-
-            migrationBuilder.CreateTable(
-                name: "challenge_tags",
-                columns: table =>
-                    new
-                    {
-                        challenge_id = table.Column<Guid>(type: "uuid", nullable: false),
-                        tag_id = table.Column<Guid>(type: "uuid", nullable: false)
-                    },
-                constraints: table =>
-                {
-                    table.PrimaryKey("pk_challenge_tags", x => new { x.challenge_id, x.tag_id });
-                    table.ForeignKey(
-                        name: "fk_challenge_tags_challenges_challenge_id",
-                        column: x => x.challenge_id,
-                        principalTable: "challenges",
-                        principalColumn: "id",
-                        onDelete: ReferentialAction.Cascade
-                    );
-                    table.ForeignKey(
-                        name: "fk_challenge_tags_tag_tags_id",
-                        column: x => x.tag_id,
-                        principalTable: "tags",
-                        principalColumn: "id",
-                        onDelete: ReferentialAction.Cascade
-                    );
-                }
-            );
-
-            migrationBuilder.CreateIndex(
-                name: "ix_challenge_tags_tags_id",
-                table: "challenge_tags",
-                column: "tag_id"
-            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(name: "challenge_tags");
-
             migrationBuilder.DropPrimaryKey(name: "pk_tags", table: "tags");
 
             migrationBuilder.DropIndex(name: "ix_tags_value", table: "tags");
