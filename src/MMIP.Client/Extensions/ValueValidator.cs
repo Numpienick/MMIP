ffcfe62a9054455f3fab1b57fc4a30dbd90c80c7
WimStienstra <w.j.stienstra@home.nl>
2023-12-08 16:53:13 +0100

    fix: changed mudcontainer width, refactoring, commented fileupload

--- src/MMIP.Client/Extensions/ValueValidator.cs
+++ src/MMIP.Client/Extensions/ValueValidator.cs
@@ -3,18 +3,13 @@ using MudBlazor;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Localization;
 using MMIP.Client.Resources;
+using MMIP.Shared.Entities;
 
 namespace MMIP.Client.Extensions
 {
-    public class CustomValidation
+    internal class ValueValidator
     {
-        [Inject]
-        private ISnackbar _snackbar { get; set; }
-
-        [Inject]
-        private IStringLocalizer<ApplicationResource> _localizer { get; set; }
-
-        internal List<ValidationResult>? Validate(string value, string validationString, Type model)
+        internal List<ValidationResult> Validate(string value, string validationString, Type model)
         {
             var context = new ValidationContext(value, null, null);
             var results = new List<ValidationResult>();
