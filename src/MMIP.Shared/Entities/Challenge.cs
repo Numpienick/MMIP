cdd38430e9876bebdc0a6de3e3758fbe80cbf25f
WimStienstra <w.j.stienstra@home.nl>
2023-12-08 14:00:23 +0100

    fix: fixed merge errors

--- src/MMIP.Shared/Entities/Challenge.cs
+++ src/MMIP.Shared/Entities/Challenge.cs
@@ -26,13 +26,10 @@ namespace MMIP.Shared.Entities
 
         [Required(ErrorMessage = "Zichtbaarheid moet gedefinieerd worden.")]
         public Visibility ChallengeVisibility { get; set; } = Visibility.VisibleToEmployees;
-        public DateTimeOffset StartDate { get; set; } = DateTimeOffset.MaxValue;
 
         public string? BannerImagePath { get; set; }
-        public string? FinalReport { get; set; }
         public Guid OrganizationId { get; set; } // TODO: add organization to creating challenges
         public Guid CurrentPhaseId { get; set; }
-        public List<Tag> Tags = new();
         public List<Phase> Phases { get; set; } = new();
     }
 }
