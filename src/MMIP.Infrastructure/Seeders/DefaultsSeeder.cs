97f18ed8f707349526c4d5c714f6ebed024e3d05
Leander Jelgersma <Leanderj123@outlook.com>
2023-12-12 14:59:28 +0100

    feat(MAT-145): added a timeline lay-out for phase visibility

--- src/MMIP.Infrastructure/Seeders/DefaultsSeeder.cs
+++ src/MMIP.Infrastructure/Seeders/DefaultsSeeder.cs
@@ -19,12 +19,12 @@ public class DefaultsSeeder : IDatabaseSeeder
 
     private async Task _seedDefaultOrganization()
     {
-        var org = new Organization
-        {
-            Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-            Name = "Default Organization",
-        };
-        await _context.Organizations.AddAsync(org);
+        //var org = new Organization
+        //{
+        //    Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+        //    Name = "Default Organization",
+        //};
+        //await _context.Organizations.AddAsync(org);
         await _context.SaveChangesAsync();
     }
 }
