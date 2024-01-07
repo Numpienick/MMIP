33895fc39bba9a2e36a984b4b8d7ff50d562f7d2
Nick Numan <nicknuman@gmail.com>
2024-01-07 22:36:01 +0100

    feat: added automapper to the project with a profile for OrganizationProfile

--- src/MMIP.Server/Program.cs
+++ src/MMIP.Server/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddDatabase(false);
 builder.Services.AddEntityServices();
 builder.Services.AddRepositories();
+builder.Services.AddAutoMapperProfiles();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
